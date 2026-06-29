f19daeadd65d2791ad33d7ef1d44f5bbc5b422d7
VenkataNarayana03 <venkatanarayana.mogili2020@vitstudent.ac.in>
2026-06-29 21:47:04 +0530

    2.REACTJS-HOL

---
+++ REACT/react2/src/App.jsx
@@ -0,0 +1,16 @@
+import Home from './Components/Home.jsx';
+import About from './Components/About.jsx';
+import Contact from './Components/Contact.jsx';
+
+function App() {
+  return (
+    <main className="portal">
+      <h1>Student Management Portal</h1>
+      <Home />
+      <About />
+      <Contact />
+    </main>
+  );
+}
+
+export default App;
---
+++ REACT/react2/src/Components/About.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <section className="message">
+        <h2>About</h2>
+        <p>Welcome to the About page of the Student Management Portal</p>
+      </section>
+    );
+  }
+}
+
+export default About;
