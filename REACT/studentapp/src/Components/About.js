589c11439c67a79a59aec015252d18bccf19edde
VenkataNarayana03 <venkatanarayana.mogili2020@vitstudent.ac.in>
2026-07-05 10:25:49 +0530

    2.React HOL

---
+++ REACT/studentapp/src/Components/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class About extends Component {
+  render() {
+    return (
+      <div>
+        <h3>About Student Management Portal</h3>
+      </div>
+    );
+  }
+}
---
+++ REACT/studentapp/src/Components/Contact.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Contact Student Management Portal</h3>
+      </div>
+    );
+  }
+}
